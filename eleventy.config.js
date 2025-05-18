6e2fbcb8bbde44d61f9b860caf0ea1b42acba7c7
nnagewad <nikin.n@gmail.com>
2025-05-18 21:33:23 +0100

    Add api to seconds filter

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import apiToISO from "./src/_filters/api-to-iso.js";
+import apiToSeconds from "./src/_filters/api-to-seconds.js";
 import apiToString from "./src/_filters/api-to-string.js";
 
 export default async function(eleventyConfig) {
   // Add filters
   eleventyConfig.addFilter('apiToISO', apiToISO);
+  eleventyConfig.addFilter('apiToSeconds', apiToSeconds);
   eleventyConfig.addFilter('apiToString', apiToString);
   
   // 11ty Image plugin
