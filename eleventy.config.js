cee2b434c1fc668e9e0cd79fd9bb49b40c7af555
nnagewad <nikin.n@gmail.com>
2025-06-07 18:34:32 +0100

    Clean it up HTML minification

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,9 +17,8 @@ export default async function(eleventyConfig) {
   eleventyConfig.addFilter('apiToTime', apiToTime);
   eleventyConfig.addFilter('updateApostrophe', updateApostrophe);
 
-  // Minify output
+  // Minify HTML output
   eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
     if( outputPath && outputPath.endsWith('.html') ) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
