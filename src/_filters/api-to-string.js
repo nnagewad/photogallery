19378743458d128c1f8a20991ffffc83fa0a6cb5
nnagewad <nikin.n@gmail.com>
2025-06-07 16:48:47 +0100

    Update how time is displayed

--- src/_filters/api-to-string.js
+++ src/_filters/api-to-string.js
@@ -3,5 +3,5 @@ import { DateTime } from "luxon";
 export default value => {
   let date = value;
   const dateTime = DateTime.fromFormat(date, "yyyy:MM:dd HH:mm:ss");
-  return dateTime.toLocaleString(DateTime.DATETIME_FULL);
+  return dateTime.toLocaleString(DateTime.DATETIME_SHORT);
 }
