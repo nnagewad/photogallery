7c812149785c5b46887a296ef469041ad7d88b01
nnagewad <nikin.n@gmail.com>
2025-06-07 14:53:17 +0100

    Generate random number for stylesheet

---
+++ src/_data/global.js
@@ -0,0 +1,6 @@
+export function random() {
+  const segment = () => {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return `${segment()}-${segment()}-${segment()}`;
+}
