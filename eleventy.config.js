eb834227fc7e61d5841052e113b4c3e9960d7f97
nnagewad <nikin.n@gmail.com>
2025-09-06 13:18:40 +0100

    Update order of plugins

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,64 +1,22 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import pluginRss from "@11ty/eleventy-plugin-rss";
+import { minify } from 'terser';
+import htmlmin from 'html-minifier-terser';
 import apiToISO from "./src/_filters/api-to-iso.js";
 import apiToDate from "./src/_filters/api-to-date.js";
 import apiToTime from "./src/_filters/api-to-time.js";
 import updateApostrophe from "./src/_filters/update-apostrophe.js";
-import { minify } from 'terser';
-import htmlmin from 'html-minifier-terser';
-import pluginRss from "@11ty/eleventy-plugin-rss";
 
 export default async function(eleventyConfig) {
-  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  // Configuration
   eleventyConfig.setUseGitIgnore(false);
   
-  // Watch SCSS files for changes
   eleventyConfig.setServerOptions({
     watch: ['./_site/css/**/*.css'],
   });
 
-  // Passthrough
-  // Passthrough copy of images for RSS feed
-  eleventyConfig.addPassthroughCopy("src/img/photos");
-  // Passthrough favicons
-  eleventyConfig.addPassthroughCopy("src/img/favicon");
-  // Passthrough open-graph images
-  eleventyConfig.addPassthroughCopy("src/img/open-graph");
-
-  // Add filters
-  eleventyConfig.addFilter('apiToISO', apiToISO);
-  eleventyConfig.addFilter('apiToDate', apiToDate);
-  eleventyConfig.addFilter('apiToTime', apiToTime);
-  eleventyConfig.addFilter('updateApostrophe', updateApostrophe);
-
-  // Inline JS
-  eleventyConfig.addNunjucksAsyncFilter('jsmin', async function (
-    code,
-    callback
-  ) {
-    try {
-      const minified = await minify(code);
-      callback(null, minified.code);
-    } catch (err) {
-      console.error('Terser error: ', err);
-      // Fail gracefully.
-      callback(null, code);
-    }
-  });
-
-  // Minify HTML output
-  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
-    if( outputPath && outputPath.endsWith('.html') ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
-    return content;
-  });
-  
-  // 11ty Image plugin
+  // Plugins (add early so they can be configured)
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["avif", "webp"],
     widths: ["auto"],
@@ -71,8 +29,43 @@ export default async function(eleventyConfig) {
     },
   });
 
-  // 11ty RSS plugin
-  eleventyConfig.addPlugin(pluginRss);
+  // Passthrough copies
+  eleventyConfig.addPassthroughCopy("src/img/photos");
+  eleventyConfig.addPassthroughCopy("src/img/favicon");
+  eleventyConfig.addPassthroughCopy("src/img/open-graph");
+
+  // Filters
+  eleventyConfig.addFilter('apiToISO', apiToISO);
+  eleventyConfig.addFilter('apiToDate', apiToDate);
+  eleventyConfig.addFilter('apiToTime', apiToTime);
+  eleventyConfig.addFilter('updateApostrophe', updateApostrophe);
+
+  // Async filters
+  eleventyConfig.addNunjucksAsyncFilter('jsmin', async function (
+    code,
+    callback
+  ) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error('Terser error: ', err);
+      callback(null, code);
+    }
+  });
+
+  // Transforms (apply last)
+  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+    if( outputPath && outputPath.endsWith('.html') ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
 
   return {
     markdownTemplateEngine: 'njk',
