cf8b24ea9c36b506c6e68f0747a31ad80599466b
nnagewad <nikin.n@gmail.com>
2025-10-18 12:36:04 +0100

    Update settings

--- src/_includes/pagefind.js
+++ src/_includes/pagefind.js
@@ -1,3 +1,8 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  new PagefindUI({ element: "#search", showSubResults: true });
+  new PagefindUI({
+    element: "#search",
+    showSubResults: true,
+    resetStyles: true,
+    addStyles: false,
+  });
 });
