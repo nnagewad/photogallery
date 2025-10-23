65dff074962d31b00f5f99efae51651c70d3b847
nnagewad <nikin.n@gmail.com>
2025-10-23 20:16:21 +0100

    modify further

--- src/_includes/update-favicon.js
+++ src/_includes/update-favicon.js
@@ -1,14 +1,11 @@
 const faviconTag = document.getElementById("favicon-tag");
 const isDark = window.matchMedia("(prefers-color-scheme: dark)");
-const isLight = window.matchMedia("(prefers-color-scheme: light)");
 
 const changeFavicon = () => {
   if (isDark.matches) faviconTag.href = "/img/favicon/favicon-dark.svg";
-  else if (isLight.matches) faviconTag.href = "/img/favicon/favicon-light.svg";
-  else faviconTag.href = "/img/favicon/favicon.svg";
+  else faviconTag.href = "/img/favicon/favicon-light.svg";
 };
 
 changeFavicon();
 
 isDark.addEventListener("change", changeFavicon);
-isLight.addEventListener("change", changeFavicon);
